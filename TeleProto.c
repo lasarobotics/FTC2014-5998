fe6e1b5f7382fcdecb864239e8ecf418ea5d92f1
lasa418@gmail.com <lasa418@gmail.com>
2014-11-24 16:29:18 -0600

    blah

--- TeleProto.c
+++ TeleProto.c
@@ -1,4 +1,3 @@
-
 #pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTMotor,  HTServo)
 #pragma config(Hubs,  S2, HTServo,  none,     none,     none)
 #pragma config(Hubs,  S3, HTMotor,  HTMotor,  none,     none)
